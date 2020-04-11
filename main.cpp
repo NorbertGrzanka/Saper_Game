9b293a6190e4fb1896307ae2a2fdec76ff62e211
Norbert Grzanka <253327@student.pwr.edu.pl>
2020-04-11 11:11:11 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,22 +7,5 @@
 
 int main()
 {
-    sf::RenderWindow win(...);
-
-    MinesweeperBoard board(7, 4, DEBUG);
-    MSSFMLView view (board);
-
-
-    board.toggleFlag(0,0);
-    board.revealField(2,3);
-
-    // rysujemy
-    while (win.isOpen())
-    {
-        // obsługa zdarzeń
-        //
-        win.clear();
-        view.draw(win);   // wywołujemy funkcję draw i dostarczamy jej okno na którym ma odbyć sie rysowanie
-        win.display();
-    }
+    
 }
