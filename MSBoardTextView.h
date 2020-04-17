4093a4b46cac670168a5a55693a8af1aada33f1d
Norbert Grzanka <253327@student.pwr.edu.pl>
2020-04-17 11:04:37 +0200

    Update MSBoardTextView.h

--- MSBoardTextView.h
+++ MSBoardTextView.h
@@ -1,17 +1,28 @@
-#ifndef UNTITLED18_MSBOARDTEXTVIEW_H
-#define UNTITLED18_MSBOARDTEXTVIEW_H
+#include <SFML/Graphics.hpp>
+#include "MinesweeperBoard.h"
+#include <string>
+
+#ifndef UNTITLED18_MSSFMLVIEW_H
+#define UNTITLED18_MSSFMLVIEW_H
+#define size_ 40
+
+class MSSFMLView {
+    MinesweeperBoard& board;
+
+    sf::Font font;
+    sf::Text text;
+    sf::RectangleShape rectangle;
 
 
-class MSBoardTextView {
-    MinesweeperBoard &board;
-private:
-    int height;
-    int width;
 public:
-  
-    MSBoardTextView(MinesweeperBoard & board )                ;
-    void display() const;
+    explicit MSSFMLView(MinesweeperBoard& board) :board(board){
+        rectangle.setSize(sf::Vector2f(size_,size_));
+        text.setFillColor(sf::Color::Yellow);
+        font.loadFromFile("arial.ttf");
+        text.setFont(font);
+    }
+    void draw (sf::RenderWindow& window);
 };
 
 
-#endif //UNTITLED18_MSBOARDTEXTVIEW_H
+#endif //UNTITLED18_MSSFMLVIEW_H
